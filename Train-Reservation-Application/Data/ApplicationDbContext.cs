0139fffd2f6ea1336f9702a59c9d8ba9ed75f5c2
Amalia Vargatu <avargatu@yahoo.com>
2021-09-14 11:21:45 +0300

    configuration to MS SQL

---
+++ Train-Reservation-Application/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Train_Reservation_Application.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+        {
+        }
+    }
+}
